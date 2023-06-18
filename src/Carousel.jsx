24c18b2dddf0e56c1f32f0e310bff72eba08a49a
Rupam Sutar <rupamsutar281998@gmail.com>
2023-06-18 17:02:10 +0530

    Building the useEffect

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,20 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {shortList, list, longList} from "./data";
 import {FaQuoteRight} from "react-icons/fa";
 import {FiChevronLeft, FiChevronRight} from "react-icons/fi";
 
 const Carousel = () => {
-  const [people, setPeople] = useState(shortList);
+  const [people, setPeople] = useState(list);
+  const [currentPerson, setCurrentPerson] = useState(0);
 
-  const prevSlide = () => {};
-  const nextSlide = () => {};
+  const prevSlide = () => {
+    setCurrentPerson((prevState) => {
+       return (prevState - 1 + people.length) % people.length;
+    })
+  };
+  const nextSlide = () => {
+    setCurrentPerson((prevState) => {
+        return (prevState + 1) % people.length;
+    })
+    
+  };
+
+  useEffect(() => {
+    const sliderId = setInterval(nextSlide, 2000);
+
+    return () => {
+        clearInterval(sliderId);
+    }
+  }, [currentPerson])
   return (
     <section className="slider-container">
-      {people.map((person) => {
+      {people.map((person, personIndex) => {
         const { id, image, name, title, quote } = person;
 
         return (
-          <article className="slide">
+          <article
+            key={personIndex}
+            className="slide"
+            style={{
+              transform: `translateX(${(personIndex - currentPerson) * 100}%)`,
+              opacity: personIndex === currentPerson ? 1 : 0,
+              visibility: personIndex === currentPerson ? "visible" : "hidden",              
+            }}
+          >
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
@@ -24,10 +50,10 @@ const Carousel = () => {
         );
       })}
 
-      <button className="prev" type="button" onCLick={prevSlide}>
+      <button className="prev" type="button" onClick={prevSlide}>
         <FiChevronLeft />
       </button>
-      <button className="next" type="button" onCLick={nextSlide}>
+      <button className="next" type="button" onClick={nextSlide}>
         <FiChevronRight />
       </button>
     </section>
